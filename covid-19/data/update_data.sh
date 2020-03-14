65afb71ebaf04370c4b8f5e227610be1a1b584db
hothman <houcemoo@gmail.com>
2020-03-14 19:18:31 +0100

    automating data update for covid-19

---
+++ covid-19/data/update_data.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv
+
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv
+
+wget https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv 
